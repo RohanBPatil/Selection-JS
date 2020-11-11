c567fa82ef0a0ce96e02366b8f8852e3216ab62a
RohanBPatil <rohanpatil14218@gmail.com>
2020-11-11 18:50:46 +0530

    Added : Program to get weekday by number

---
+++ NumberToWeekday.js
@@ -0,0 +1,19 @@
+var prompt = require('prompt-sync')();
+let num = prompt('Enter number for weekday: ');
+if (num == 1) {
+    console.log("Sunday");
+} else if (num == 2) {
+    console.log("Monday");
+} else if (num == 3) {
+    console.log("Tuesday");
+} else if (num == 4) {
+    console.log("Wednesday");
+} else if (num == 5) {
+    console.log("Thursday");
+} else if (num == 6) {
+    console.log("Friday");
+} else if (num == 7) {
+    console.log("Saturday");
+} else {
+    console.log("Entered wrong number");
+}
